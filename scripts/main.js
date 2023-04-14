13944794de1b826d21c2defc16f99d2e8db320e8
chikien111 <38467294+chikien111@users.noreply.github.com>
2023-04-14 22:02:39 +0700

    update new page

---
+++ scripts/main.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(function ($) {
+ 
+  var App = App || {};
+
+  //---MAIN----
+  $(function () {
+      App.Site.playvideo();
+      App.Site.semaintic();
+      App.Site.owl();
+      App.Site.fancybox();
+  });
+
+  //--All site
+    App.Site = function(){
+        var playvideo = function(){
+            $("#playvideo").click(function(){
+                $("#video1")[0].src += "?autoplay=1";
+                $(this).addClass("md-hide");
+            });
+        }
+        var semaintic = function(){
+            
+            $('.ui.dropdown')
+              .dropdown()
+            ;
+        }
+        var owl = function(){
+            
+            $('.md-content .owl-carousel').owlCarousel({
+                loop:true,
+                margin:20,
+                nav:true,
+                autoplay:true,
+                responsive:{
+                    0:{
+                        items:1.2
+                    },
+                    600:{
+                        items:2.2
+                    },
+                    1000:{
+                        items:4.2
+                    }
+                }
+            })
+        }
+        var fancybox = function(){
+            // $("#modal-sucess-form").fancybox().trigger('click');
+             // $("#modal-sucess-form-2").fancybox().trigger('click');
+        }
+
+
+        return{
+            playvideo:playvideo,
+            semaintic:semaintic,
+            owl:owl,
+            fancybox:fancybox,
+        };
+
+    }(); 
+
+  //--End All site
+
+
+    
+   
+});    
+
+
