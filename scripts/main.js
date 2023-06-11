1ea0426ddc3b7921eeb0d8ae6748d00588913b65
Kubia <tranddong@gmail.com>
2023-06-11 21:46:31 +0700

    google sheet

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-jQuery(document).ready(function ($) {
- 
   var App = App || {};
 
   //---MAIN----
@@ -10,6 +8,9 @@ jQuery(document).ready(function ($) {
       App.Site.fancybox();
       App.Site.scrolltoTop();
       App.Site.scrollDiv();
+
+      //
+      App.Site.submitValidate();
   });
 
   //--All site
@@ -114,23 +115,152 @@ jQuery(document).ready(function ($) {
             });
         }
 
+        // a Dong
+        var submitValidate = function(){
+            getCurrentDate();
+
+            var formRegister = $('form#registerForm');
+            if (formRegister.length < 1) {
+                return;
+            }
+            $.validator.addMethod("validatePhone", function (value, element) {
+                var flag = false;
+                var phone = value;
+                phone = phone.replace('(+84)', '0');
+                phone = phone.replace('+84', '0');
+                phone = phone.replace('0084', '0');
+                phone = phone.replace(/ /g, '');
+                if (phone != '') {
+                    var firstNumber = phone.substring(0, 3);
+                    // var validNumber = ["086","088","096","097","098","032","033","034","035","036","037","038","039","089","090","093","070","079","077","076","078","091","094","083","084","085","081","082","092","056","058"];
+                    var validNumber = ["032", "033", "034", "035", "036", "037", "038", "039", "070", "079", "077", "076", "078", "083", "084", "085", "081", "082", "056", "058", "059", "086", "088", "089", "092", "099", "096", "091", "090", "056", "097", "094", "093", "058", "098", "070", "095"];
+                    if ((jQuery.inArray(firstNumber,validNumber)!='-1') && phone.length == 10) {
+                        if (phone.match(/^\d{10}/)) {
+                            flag = true;
+                        }
+                    }
+                }
+                return flag;
+            }, "Hãy nhập đúng định dạng số điện thoại");
+            $.validator.addMethod("regex",
+                function(value, element, regexp) {
+                    var check = false;
+                    return this.optional(element) || regexp.test(value);
+                },
+                "Vui lòng nhập đúng định dạng");
+            var validForm = formRegister.validate({
+                rules: {
+                    name: {
+                        required: true,
+                    },
+                    email:{
+                        required: true,
+                        email:true
+                    },
+                    phone: {
+                        required: true,
+                        validatePhone:true
+                    },
+                    company: {
+                        required: true
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Vui lòng nhập họ tên"
+                    },
+                    phone: {
+                        required: "Vui lòng nhập số điện thoại",
+                        validatePhone:"Sai định dạng",
+                        remote:"Số điện thoại đã được đăng ký rồi"
+                    },
+                    email: {
+                        required: "Vui lòng nhập email",
+                        email: "Sai định dạng",
+                    },
+                    company: {
+                        required: "Vui lòng nhập tên công ty"
+                    }
+                },
+                errorElement : 'p',
+                errorClass: 'error',
+                errorPlacement: function(error, element) {
+                    error.insertAfter(element);
+                },
+                highlight: function (element, errorClass) {
+                    $(element).closest('.form-group').addClass('error');
+                },
+                unhighlight: function (element, errorClass) {
+                    $(element).closest('.form-group').removeClass('error');
+                },
+                success: function() {
+                    // 'this' refers to the form
+                    var errors = validForm.numberOfInvalids();
+                    if(errors==0){
+                        if($('input[name=\'rule\']').is(':checked')){
+                            $(':input[class="submit"]').prop('disabled', false);
+                        }
+                    }
+                }
+            });
+        };
+
+        var clickSubmit = true;
+        var registerSubmit = function(){
+            if ($('form#registerForm').valid()) {
+                if (clickSubmit) {
+                    var data = $('form#registerForm').serializeArray();
+                    clickSubmit = false;
+
+                    $.ajax({
+                        type : 'GET',
+                        url : 'https://script.google.com/macros/s/AKfycbzvbGD0KjH1WoA_QNQ6tDw2jSUHFoBNO5JUTKxXJumxGjhGskpk3LASi5Z7McA35-c/exec',
+                        dataType:'json',
+                        crossDomain : true,
+                        data : data,
+                        success : function(data)
+                        {
+                            clickSubmit = true;
+
+                            if(data == 'false')
+                            {
+                                alert('ERROR, Please try again later!');
+                            }else{
+                                $('form#registerForm').find('input[type="text"], input[type="tel"], input[type="email"]').val(''); //--reset form
+
+                                $("#modal-sucess-form").fancybox().trigger('click');
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
+        var getCurrentDate = function() {
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+    
+            today = mm + '/' + dd + '/' + yyyy;
+            
+            $('#current-date').val(today);
+        }
+
         return{
-            toggle_slide:toggle_slide,
-            semaintic:semaintic,
-            owl:owl,
-            fancybox:fancybox,
-            scrolltoTop:scrolltoTop,
-            scrollDiv:scrollDiv,
+            toggle_slide: toggle_slide,
+            semaintic: semaintic,
+            owl: owl,
+            fancybox: fancybox,
+            scrolltoTop: scrolltoTop,
+            scrollDiv: scrollDiv,
+            submitValidate:submitValidate,
+            registerSubmit: registerSubmit
         };
 
     }(); 
 
   //--End All site
-
-
-    
-   
-});    
 
 
 
